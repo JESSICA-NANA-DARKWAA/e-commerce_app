9ad3f566b8a38edf044d2977891aa5150aab40c5
JESSICA-NANA-DARKWAA <jessica.darkwaa@amalitech.org>
2023-12-05 08:05:59 +0000

    new codes

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../componenents/navbars/Navbar2";
 import "./Checkout.css";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../AppContext";
+import { MdStoreMallDirectory } from "react-icons/md";
 
 const Checkout = () => {
+  const { cart } = useContext(AppContext);
+
   return (
     <Layout>
       <div className="circle-container">
@@ -14,9 +19,57 @@ const Checkout = () => {
       </div>
       <h2>Shipping Address</h2>
       <div className="identity-container">
-        <p> Robert Elinam</p>
         <button className="cart-button">Change</button>
+        <div className="secondary-text">
+          <p>
+            Western Region Sekondi Takoradi Metro Takoradi 17/8 old john saba
+            road.
+          </p>
+        </div>
       </div>
+      <div className="cart-view-container">
+        {cart.map((item) => {
+          return (
+            <div className="cart-card" key={item.id}>
+              <img src={item.image} alt="product" />
+
+              <div className="title-box">
+                {item.title}
+                <div className="title-text">
+                  <MdStoreMallDirectory />
+                  Seller: James cottage
+                  <br />
+                  <p>2 Available</p>
+                </div>
+              </div>
+              <div className="price">GH₵{item.price}</div>
+              <div className="incresement-box">
+                <Link to="/Cart" className="">
+                  <button className="remove-btn">-</button>
+                </Link>
+                <Link to="/Cart" className="">
+                  <button className="num-btn">1</button>
+                </Link>
+                <Link to="/Cart" className="">
+                  <button className="add-btn">+</button>
+                </Link>
+              </div>
+              <div className="select-box">
+                <Link to="/Products" className="remove-box">
+                  <button className="remove-now-button">Remove</button>
+                </Link>
+                <Link to="/Payment" className="remove-box">
+                  <button className="cart-button">Buy Now</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button className="check-price-button">GH₵</button>
+      <Link to="/Payment">
+        <button className="check-btn">Confirm Order</button>
+      </Link>
     </Layout>
   );
 };
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,10 +1,64 @@
 import React from "react";
 import Layout from "../../componenents/navbars/Navbar2";
+import "./Payment.css";
 
 const Payment = () => {
   return (
     <Layout>
-      <div>Payment</div>
+      <div className="payment-cirlce-container">
+        <div className="circle-container">
+          <button className="number-circle1">1</button>
+          <button className="line-int"></button>
+          <button className="number-circle2">2</button>
+          <button className="line-int"></button>
+          <button className="number-circle3">3</button>
+        </div>
+      </div>
+      <h2>Payment Method</h2>
+      <div className="payment-option-box">
+        <div className="payment-box">
+          <div className="option-btn-container">
+            <button className="option-btn">
+              {" "}
+              <h3>Bank</h3>
+              <p> Fidelity Bank</p>
+            </button>
+            <button className="option-btn">
+              <h3>Card Holder</h3>
+              <p>Robert Elinam</p>
+            </button>
+            <button className="option-btn">
+              {" "}
+              <h3> Branch</h3>
+              <p>Central Capital</p>
+            </button>
+            <button className="option-btn">
+              <h3>Card Number</h3>
+              <p>1 2 3 4 5 6 7 8 9 1 2 3 4</p>
+            </button>
+            <button className="option-btn">
+              <h3>cvc</h3>
+              <p>123</p>
+            </button>
+            <button className="option-btn">
+              <h3> Expiry Date </h3>
+              <p>07/23</p>
+            </button>
+          </div>
+        </div>
+        <div className="summary-box">
+          Summary
+          <div className="summary-box-container">
+            <p>items</p>
+            <p>1</p>
+            <p> PaymentMethod</p>
+            <p>Visa</p>
+            <p>Amount</p>
+            <p>GH₵</p>
+          </div>
+        </div>
+      </div>
+      <button className="pay-button">Pay</button>
     </Layout>
   );
 };
