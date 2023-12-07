3938f370559ff41f67cb4baeb6b7d1b1a407dd17
JESSICA-NANA-DARKWAA <jessica.darkwaa@amalitech.org>
2023-12-07 11:07:07 +0000

    new codes

--- src/componenents/layout/Layout.jsx
+++ src/componenents/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../navbars/Navbar";
 import SecondaryNavbar from "../navbars/SecondaryNavbar";
 import Footer from "../footer/Footer";
+import "./Layout.css";
 
 const Layout = ({ pageNumber, children }) => {
   return (
